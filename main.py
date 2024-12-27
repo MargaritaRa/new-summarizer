dc481c287919a164de374e2adcb15a1a4464aa31
Margarita Ramirez <margarita26.mr@gmail.com>
2024-12-27 10:34:36 -0500

    adding api

--- main.py
+++ main.py
@@ -1,15 +1,64 @@
 import openai
+import os
 from dotenv import find_dotenv, load_dotenv
 import time
 import logging
 from datetime import datetime
+import requests
+import json
 
 load_dotenv()
 
-news_api_key = os.environ.get("")
+news_api_key = os.environ.get("NEWS_API_KEY")
+
 client = openai.OpenAI()
 model = "gpt-3.5-turbo-16k"
 
+def get_news(topic):
+    url = (
+        f"https://newsapi.org/v2/everything?q={topic}&apikey={news_api_key}&pageSize=5"
+    )
+
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            # indent just makes things look better
+            news = json.dumps(response.json(), indent=4)
+            news_json = json.loads(news)
+
+            data = news_json
+
+            # Access all the fiels == loop through
+            status = data["status"]
+            total_results = data ["totalResults"]
+            articles = data["articles"]
+            final_news = []
+
+            # Loop through the articles
+            for article in articles:
+                source_name = article["source"]["name"]
+                author = article["author"]
+                title = article["title"]
+                description = article["description"]
+                url = article["url"]
+                content = article["content"]
+                title_description = f"""
+                    Title: {title},
+                    Author: {author},
+                    Source: {source_name},
+                    Description: {description},
+                    URL: {url}
+
+                """
+                final_news.append(title_description)
+
+            return final_news
+        else:
+            return []
+
+        
+    except requests.exceptions.RequestException as e:
+        print("Error occured duriong API Request", e)
 
 
 
@@ -18,82 +67,11 @@ model = "gpt-3.5-turbo-16k"
 
 
 
+def main():
+    news = get_news("bitcoin")
+    print(news[0])
 
-# # # ==  Create our Assistant (Uncomment this to create your assistant) ==
-# personal_trainer_assis = client.beta.assistants.create(
-#     name="Personal Trainer",
-#     instructions="""You are the best personal trainer and nutritionist who knows how to get clients to build lean muscles.\n
-#      You've trained high-caliber athletes and movie stars. """,
-#     model=model,
-# )
-# asistant_id = personal_trainer_assis.id
-# print(asistant_id)
 
-# # === Thread (uncomment this to create your Thread) ===
-# thread = client.beta.threads.create(
-#     messages=[
-#         {
-#             "role": "user",
-#             "content": "How do I get started working out to lose fat and build muscles?",
-#         }
-#     ]
-# )
-# thread_id = thread.id
-# print(thread_id)
 
-# == Hardcode our ids ===
-asistant_id = "asst_bWLYUFJ7VkVES9OOYBrpOzB0"
-thread_id = "thread_FTiTaOQrqsDlVq0ngNjaJ6xT"
-
-# ==== Create a Message ====
-message = "HOw much water should I drink in a day to get healthier?"
-message = client.beta.threads.messages.create(
-    thread_id=thread_id, 
-    role="user", 
-    content=message
-)
-# === Run our Assistant ===
-run = client.beta.threads.runs.create(
-    thread_id=thread_id,
-    assistant_id=asistant_id,
-    instructions="Please address the user as James Bond",
-)
-
-def wait_for_run_completion(client, thread_id, run_id, sleep_interval=5):
-    """
-    Waits for a run to complete and prints the elapsed time.:param client: The OpenAI client object.
-    :param thread_id: The ID of the thread.
-    :param run_id: The ID of the run.
-    :param sleep_interval: Time in seconds to wait between checks.
-    """
-    while True:
-        try:
-            run = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run_id)
-            if run.completed_at:
-                elapsed_time = run.completed_at - run.created_at
-                formatted_elapsed_time = time.strftime(
-                    "%H:%M:%S", time.gmtime(elapsed_time)
-                )
-                print(f"Run completed in {formatted_elapsed_time}")
-                logging.info(f"Run completed in {formatted_elapsed_time}")
-                # Get messages here once Run is completed! #
-                messages = client.beta.threads.messages.list(thread_id=thread_id)
-                last_message = messages.data[0]
-                response = last_message.content[0].text.value
-                print(f"Assistant Response: {response}")
-                break
-        except Exception as e:
-            logging.error(f"An error occurred while retrieving the run: {e}")
-            break
-        logging.info("Waiting for run to complete...")
-        time.sleep(sleep_interval)
-
-#  === Run ===
-wait_for_run_completion(client=client, thread_id=thread_id, run_id=run.id)
-
-#  === Steps --- Logs ===
-run_steps = client.beta.threads.runs.steps.list(
-    thread_id=thread_id,
-    run_id=run.id
-)
-print(f"Steps---> {run_steps.data[0]}")
+if __name__ == "__main__":
+    main()
