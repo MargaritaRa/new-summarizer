e9e48cdecd69659bfe0f85c30471eb3392a1b911
Margarita Ramirez <margarita26.mr@gmail.com>
2025-02-17 11:03:11 -0500

    completed

--- main.py
+++ main.py
@@ -69,27 +69,25 @@ class AssistantManager:
     def __init__(self, model: str = model):
         self.client = client
         self.model = model
-        self.assistant = None,
-        self.thread = None,
+        self.assistant = None
+        self.thread = None
         self.run = None
         self.summary = None
 
-        # Retrieve exsisting assistant and thread if IDs are already made
+        # Retrieve existing assistant and thread if IDs are already set
         if AssistantManager.assistant_id:
             self.assistant = self.client.beta.assistants.retrieve(
                 assistant_id=AssistantManager.assistant_id
             )
         if AssistantManager.thread_id:
-            self.thread_id = self.client.beta.threads.retrieve(
+            self.thread = self.client.beta.threads.retrieve(
                 thread_id=AssistantManager.thread_id
             )
+
     def create_assistant(self, name, instructions, tools):
         if not self.assistant:
             assistant_obj = self.client.beta.assistants.create(
-                name = name,
-                instructions = instructions,
-                tools = tools,
-                model = self.model
+                name=name, instructions=instructions, tools=tools, model=self.model
             )
             AssistantManager.assistant_id = assistant_obj.id
             self.assistant = assistant_obj
@@ -97,7 +95,7 @@ class AssistantManager:
 
     def create_thread(self):
         if not self.thread:
-            thread_obj = self.create.beta.threads.create()
+            thread_obj = self.client.beta.threads.create()
             AssistantManager.thread_id = thread_obj.id
             self.thread = thread_obj
             print(f"ThreadID::: {self.thread.id}")
@@ -105,35 +103,35 @@ class AssistantManager:
     def add_message_to_thread(self, role, content):
         if self.thread:
             self.client.beta.threads.messages.create(
-                thread_id=self.thread.id,
-                role=role,
-                content=content
+                thread_id=self.thread.id, role=role, content=content
             )
 
     def run_assistant(self, instructions):
         if self.thread and self.assistant:
-            self.run = self.client.beta.threads.runds.create(
+            self.run = self.client.beta.threads.runs.create(
                 thread_id=self.thread.id,
                 assistant_id=self.assistant.id,
-                instructions=instructions
+                instructions=instructions,
             )
 
     def process_message(self):
         if self.thread:
-            messages = self.client.beta.threads.messages.list(
-                thread_id=self.thread.id
-            )
+            messages = self.client.beta.threads.messages.list(thread_id=self.thread.id)
             summary = []
 
-            last_massage = messages.data[0]
-            role = last_massage.role
-            response = last_massage.content[0].text.value
+            last_message = messages.data[0]
+            role = last_message.role
+            response = last_message.content[0].text.value
             summary.append(response)
 
             self.summary = "\n".join(summary)
-            print(f"Summary-------> {role.capitalize()}: ==> {response}")
-        return summary
-    
+            print(f"SUMMARY-----> {role.capitalize()}: ==> {response}")
+
+            # for msg in messages:
+            #     role = msg.role
+            #     content = msg.content[0].text.value
+            #     print(f"SUMMARY-----> {role.capitalize()}: ==> {content}")
+
     def call_required_functions(self, required_actions):
         if not self.run:
             return
@@ -145,33 +143,30 @@ class AssistantManager:
 
             if func_name == "get_news":
                 output = get_news(topic=arguments["topic"])
-                print(f"STUFFFFFF;;;;; {output}")
+                print(f"STUFFFFF;;;;{output}")
                 final_str = ""
                 for item in output:
                     final_str += "".join(item)
-                
+
                 tool_outputs.append({"tool_call_id": action["id"], "output": final_str})
             else:
                 raise ValueError(f"Unknown function: {func_name}")
 
         print("Submitting outputs back to the Assistant...")
         self.client.beta.threads.runs.submit_tool_outputs(
-            thread_id=self.thread.id,
-            run_id=self.run.id,
-            tool_outputs=tool_outputs
+            thread_id=self.thread.id, run_id=self.run.id, tool_outputs=tool_outputs
         )
-    
+
     # for streamlit
     def get_summary(self):
         return self.summary
-    
-    def wait_for_completed(self):
+
+    def wait_for_completion(self):
         if self.thread and self.run:
             while True:
                 time.sleep(5)
                 run_status = self.client.beta.threads.runs.retrieve(
-                    thread_id=self.thread.id,
-                    run_id=self.run.id
+                    thread_id=self.thread.id, run_id=self.run.id
                 )
                 print(f"RUN STATUS:: {run_status.model_dump_json(indent=4)}")
 
@@ -183,30 +178,70 @@ class AssistantManager:
                     self.call_required_functions(
                         required_actions=run_status.required_action.submit_tool_outputs.model_dump()
                     )
-    
+
+    # Run the steps
     def run_steps(self):
         run_steps = self.client.beta.threads.runs.steps.list(
-            thread_id=self.thread.id,
-            run_id=self.run.id
+            thread_id=self.thread.id, run_id=self.run.id
         )
+        print(f"Run-Steps::: {run_steps}")
+        return run_steps.data
 
-        print(f"RUN-STEPS::: {run_steps}")
 
 def main():
     # news = get_news("bitcoin")
     # print(news[0])
 
     manager = AssistantManager()
-    #Streamlit interface
+
+    # Streamlit interface
     st.title("News Summarizer")
 
     with st.form(key="user_input_form"):
-            instructions = st.text_input("Enter topic:")
-            submit_button = st.form_submit_button(label="Run Assistant")
+        instructions = st.text_input("Enter topic:")
+        submit_button = st.form_submit_button(label="Run Assistant")
 
-            if submit_button:
-                manager.create_assistant()
+        if submit_button:
+            manager.create_assistant(
+                name="News Summarizer",
+                instructions="You are a personal article summarizer Assistant who knows how to take a list of article's titles and descriptions and then write a short summary of all the news articles",
+                tools=[
+                    {
+                        "type": "function",
+                        "function": {
+                            "name": "get_news",
+                            "description": "Get the list of articles/news for the given topic",
+                            "parameters": {
+                                "type": "object",
+                                "properties": {
+                                    "topic": {
+                                        "type": "string",
+                                        "description": "The topic for the news, e.g. bitcoin",
+                                    }
+                                },
+                                "required": ["topic"],
+                            },
+                        },
+                    }
+                ],
+            )
+            manager.create_thread()
 
+            # Add the message and run the assistant
+            manager.add_message_to_thread(
+                role="user", content=f"summarize the news on this topic {instructions}?"
+            )
+            manager.run_assistant(instructions="Summarize the news")
+
+            # Wait for completions and process messages
+            manager.wait_for_completion()
+
+            summary = manager.get_summary()
+
+            st.write(summary)
+
+            st.text("Run Steps:")
+            st.code(manager.run_steps(), line_numbers=True)
 
 
 if __name__ == "__main__":
